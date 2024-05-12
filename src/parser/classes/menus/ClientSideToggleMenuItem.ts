29935a607748da0c20570bf2772a6005281b83e9
XqWyZ <xqwyz@arcor.de>
2024-05-12 13:49:30 +0200

    Add new types

--- src/parser/classes/menus/ClientSideToggleMenuItem.ts
+++ src/parser/classes/menus/ClientSideToggleMenuItem.ts
@@ -1,5 +1,6 @@
 import { YTNode } from '../../helpers.js';
 import type { RawNode } from '../../index.js';
+import Text from '../misc/Text.js';
 import NavigationEndpoint from '../NavigationEndpoint.js';
 
 export default class ClientSideToggleMenuItem extends YTNode {
